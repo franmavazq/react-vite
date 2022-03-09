4d644107551218f4603e14d918b735b7d43c682f
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-03-09 17:19:32 -0300

    feat: :sparkles: Introduction to template string
    Introduction to template string

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,2 +1,16 @@
+// console.log('Hola Mundo');
 
-console.log('Hola Mundo');
+const nombre = 'Francisco';
+const apellido = 'Mancuello';
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+console.log(nombreCompleto);
+
+// No llama nombre por ser un scope diferente
+function getSaludo(nombre){
+  return 'Hola ' + nombre;
+}
+
+console.log(`Este es un texto en template string: ${getSaludo(nombre)}`)
