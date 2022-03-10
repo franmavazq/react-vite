79743efb5d3aa9dbc1278de2e1751e3ede792aa3
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-03-09 21:41:50 -0300

    feat: :sparkles: Introduction to object literals
    Introduction to object literals

--- 02-intro-javascript/src/bases/02-temple-string.js
+++ 02-intro-javascript/src/bases/02-temple-string.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundo');
+// Template string ``
+
+const nombre = 'Francisco';
+const apellido = 'Mancuello';
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+console.log(nombreCompleto);
+
+// No llama nombre por ser un scope diferente
+function getSaludo(nombre){
+  return 'Hola ' + nombre;
+}
+
+console.log(`Este es un texto en template string: ${getSaludo(nombre)}`)
