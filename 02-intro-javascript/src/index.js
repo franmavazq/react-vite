077887bad48345e4ce625737dd5ed4ace6d0fcc1
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-03-13 14:36:46 -0300

    feat: :sparkles: Import, export, y funciones comunes de arreglos
    Import, export, y funciones comunes de arreglos

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,16 @@
+import { heroes } from './data/heroes';
+
+// const getHeroeById = ( id) => {
+//   return heroes.find((heroe) => {
+//     if( heroe.id === id ) {
+//       return heroe;
+//     }else {
+//       return null;
+//     }
+//   });
+// }
+const getHeroeById = ( id ) => heroes.find( ( heroe ) => heroe.id === id );
+console.log( getHeroeById( 2 ));
+
+const getHeroesByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner );  
+console.log( getHeroesByOwner( 'Marvel' ));
