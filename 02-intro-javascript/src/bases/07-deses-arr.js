b17697bca158df6d5e70cdc3f255d6a4b898bc8a
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-03-12 23:45:21 -0300

    feat: Desestructuracion de arrelgos
    Desestructuracion de arrelgos

---
+++ 02-intro-javascript/src/bases/07-deses-arr.js
@@ -0,0 +1,29 @@
+// Desestructuracion de arreglos
+// Asignacion de arreglos
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+
+// console.log( personajes[0] );
+// console.log( personajes[1] );
+// console.log( personajes[2] );
+
+const [ , , p3] = personajes; //Retorna Trunks
+console.log(p3);
+
+const retornaArreglo = () => {
+    return ['ABC', 123];
+}
+const [letras, numeros] = retornaArreglo();
+console.log(letras, numeros);
+
+//Tarea
+// 1. El primer valor del arreglo se llamara nombre
+// 2. Se llamara setNombre
+const useState = (valor) => {
+    return [valor, () => {
+        console.log('Hola Mundo');
+    }];
+}
+const [nombre, setNombre] = useState('Francisco');
+
+console.log(nombre);
+setNombre();
