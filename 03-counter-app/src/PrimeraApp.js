f412a5ad9793caff8b6ea285d477e54bccb5b740
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-05-02 19:40:54 -0400

    feat: communication between components

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -3,12 +3,13 @@
 // import React, { Fragment } from 'react';
 import React from 'react';
 
-const PrimeraApp = () => {
-  const saludo = 'Hola mundo';
+// Recibimos la props que recibimos de index y realizamos una desestructuracion de elementos enviados al componente, para evitar colocar props.saludo
+// Ni se crea una constante adicional.
+const PrimeraApp = ( { saludo='Hola mundo' } ) => {
 
   return (
     <>
-      <h1> {saludo} </h1>
+      <h1> { saludo } </h1>
       {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
       <p>Este es un parrafo</p>
     </>
