68bad72e2d73cef0f186cd6a1c9d3867901b4776
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-05-11 22:33:33 -0400

    refactor: Migrate to react 18

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,21 @@
+// ANTES
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // import PrimeraApp from "./PrimeraApp";
+// import CounterApp from "./CounterApp";
+// import './index.css';
+
+// const divRoot = document.getElementById("root");
+// ReactDOM.render(<CounterApp value= { 10 } />, divRoot);
+
+
+// REACT +18
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 // import PrimeraApp from "./PrimeraApp";
 import CounterApp from "./CounterApp";
-
 import './index.css';
 
 const divRoot = document.getElementById("root");
-
-ReactDOM.render(<CounterApp value= { 10 } />, divRoot);
+const root = createRoot(divRoot);
+root.render(<CounterApp value= { 10 } />);
