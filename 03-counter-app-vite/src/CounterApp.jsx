9e5bd12a0750163d39cafa4273f38375455bd61f
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2022-07-09 18:10:29 -0400

    useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
   const handleAdd = () => {
-    console.log('+1')
-    value = 2000
-    console.log(value)
+    setCounter(counter + 1);
   }
+
   return (
     <>
       <h1>Counter App</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
       <button onClick={handleAdd}>
         <span>+1</span>
       </button>
